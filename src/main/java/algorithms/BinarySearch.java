d88c7014cc41f4f97858129b751112206f528537
MarcoLightyear <marcoemilio862@gmail.com>
2024-03-28 19:34:08 -0600

    added binary search algorithm

---
+++ src/main/java/algorithms/BinarySearch.java
@@ -0,0 +1,22 @@
+package algorithms;
+
+public class BinarySearch {
+    public int search(int[] array, int value) {
+        int lowerBound = 0;
+        int upperBound = array.length - 1;
+        do {
+            int midpoint = (upperBound + lowerBound) / 2;
+            int valueAtMidpoint = array[midpoint];
+            if(value < valueAtMidpoint){
+                upperBound = midpoint - 1;
+            }
+            else if(valueAtMidpoint > value){
+                lowerBound = midpoint + 1;
+            }
+            else if(valueAtMidpoint == value){
+                return midpoint;
+            }
+        } while (lowerBound <= upperBound);
+        return -1;
+    }
+}
