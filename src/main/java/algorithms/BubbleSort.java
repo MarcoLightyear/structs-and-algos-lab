b7c7cf1276da2f9552ef4fc786e82a2d313734ef
MarcoLightyear <marcoemilio862@gmail.com>
2024-03-30 14:29:32 -0600

    refactor: changed algorithm for clearer code

--- src/main/java/algorithms/BubbleSort.java
+++ src/main/java/algorithms/BubbleSort.java
@@ -2,14 +2,19 @@ package algorithms;
 
 public class BubbleSort {
     public void sort(int[] array) {
-        for (int i = 0; i < array.length - 1; i++) {
-            for (int j = 0; j < array.length - i - 1; j++) {
-                if (array[j] > array[j + 1]) {
-                    int tmp = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = tmp;
+        int indexUntilSorted = array.length - 1;
+        boolean sorted = false;
+        while(!sorted){
+            sorted = true;
+            for(int i = 0; i<indexUntilSorted; i++){
+                if(array[i] > array[i+1]){
+                    int tmp = array[i];
+                    array[i] = array[i+1];
+                    array[i+1] = tmp;
+                    sorted = false;
                 }
             }
+            indexUntilSorted--;
         }
     }
 
